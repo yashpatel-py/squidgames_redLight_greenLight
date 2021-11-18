57d35f03277d51a1ff6fa7e32bbedc38f8fed77f
yash-2115 <mailforyash04@gmail.com>
2021-11-18 10:21:56 +0530

    Added dall 3D model with the help of three.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,38 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+// Instantiate a loader
+const loader = new THREE.GLTFLoader();
+
+loader.load(
+    '../models/scene.gltf',
+    function (gltf) {
+        scene.add( gltf.scene );
+        gltf.scene.scale.set(0.4, 0.4, 0.4);
+    },
+);
+
+function animate() {
+    renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+}
+animate();
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
